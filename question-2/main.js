0291737adaf9cebe8ed1207a60c4298dd3933ffe
Grow-with-Talha <alit83219@gmail.com>
2024-03-10 20:50:22 -0700

    question2 solved

---
+++ question-2/main.js
@@ -0,0 +1,4 @@
+var name2 = 'abdul rehman';
+console.log(name2.toUpperCase());
+name2 = 'bHATti';
+console.log(name2.toLowerCase());
---
+++ question-2/main.ts
@@ -0,0 +1,5 @@
+ let name2:string='abdul rehman';
+ console.log(name2.toUpperCase());
+
+ name2='bHATti'
+console.log(name2.toLowerCase());
